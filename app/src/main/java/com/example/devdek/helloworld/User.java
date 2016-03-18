a5f127ae833cf02feaa86aa5ded72d1d62ae67c5
zodi <zoditoo@gmail.com>
2016-03-18 03:17:30 -0400

    added user Class

---
+++ app/src/main/java/com/example/devdek/helloworld/User.java
@@ -0,0 +1,25 @@
+package com.example.devdek.helloworld;
+
+/**
+ * Created by onepunch on 3/18/2016.
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
